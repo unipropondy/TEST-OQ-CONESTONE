628a93315053f0962b3340be4f8741124d91ad48
bavani18 <bavaniabavani18@gmail.com>
2026-08-18 11:07:30 +0530

    test qr

--- src/Configs/api.js
+++ src/Configs/api.js
@@ -1,4 +1,4 @@
-// export const BASE_URL = "https://new-qrcode-production.up.railway.app"; //std
+export const BASE_URL = "https://test-oq-conestone-production.up.railway.app"; //std
 
 // export const BASE_URL = "http://localhost:5000";
 
